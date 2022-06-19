83876efdae89fa9a4b76526850e845b39ac2c3b0
Ruan Scherer <ruan.scherer@outlook.com>
2022-06-19 12:04:33 -0300

    feat: create sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,45 @@
-export default function Home() {
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bgColor="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing={4}>
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+          />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt={6}
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
